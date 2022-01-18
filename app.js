d94bd6a34219953f4675c25e05be51cceda487ad
ibnukurnia <ibnubudi16@gmail.com>
2022-01-18 16:34:28 +0700

    init commit

--- app.js
+++ app.js
@@ -1,57 +1,13 @@
-const game = () => {
-  let player1Score = 0;
-  let player2Score = 0;
-  alert(`Rules : 
-    Angka Yang dimasukkan Harus 1-3.
-    Tidak Boleh Memasukkan Angka yang Sama.
-    Tidak Boleh Memasukkan Huruf.
-    Best of 5`);
-  for (let index = 0; index <= 5; index++) {
-    let guessNumber = Math.floor(Math.random() * 3) + 1;
-    let player1 = prompt("player1 masukkan Tebakan");
+nilai = prompt("Masukkan Nilai");
 
-    if (isNaN(player1)) {
-      while (isNaN(player1)) {
-        player1 = prompt("Input Harus Angka");
-      }
-    }
-    if (parseInt(player1) > 3) {
-      while (parseInt(player1) > 3) {
-        player1 = prompt("Angka Harus 1-3");
-      }
-    }
-
-    let player2 = prompt("player2 masukkan Tebakan");
-    if (parseInt(player1) === parseInt(player2)) {
-      while (parseInt(player1) === parseInt(player2)) {
-        player2 = prompt("Angka Sudah Dipilih");
-      }
-    }
-    if (isNaN(player2)) {
-      while (isNaN(player2)) {
-        player2 = prompt("Input Harus Angka");
-      }
-    }
-    if (parseInt(player2) > 3) {
-      while (parseInt(player2) > 3) {
-        player2 = prompt("Angka Harus 1-3");
-      }
-    }
-
-    if (parseInt(player1) === guessNumber) {
-      alert("Player 1 Win!");
-      alert(`Score
-      Player 1 = ${++player1Score} Player 2 = ${player2Score}`);
-    } else if (parseInt(player2) === guessNumber) {
-      alert("Player 2 Win!");
-      alert(`Score 
-      Player 1 = ${player1Score} Player 2 = ${++player2Score}`);
-    } else {
-      alert("Tidak Ada Yang Benar, Hasil Seri");
-      alert(`Score
-      Player 1 = ${player1Score} Player 2 = ${player2Score}`);
-    }
-  }
-};
-
-game();
+if (nilai >= 80 && nilai <= 100) {
+  alert("Nilai anda A");
+} else if (nilai >= 60 && nilai < 80) {
+  alert("Nilai anda B");
+} else if (nilai >= 40 && nilai < 60) {
+  alert("Nilai anda C");
+} else if (nilai >= 20 && nilai < 60) {
+  alert("Nilai anda D");
+} else {
+  alert("Nilai anda E");
+}
